31dee0f0eacbac8450398b4a64e074419ea3ea47
Teymur <baratov.teymur@yandex.ru>
2023-10-11 09:03:04 +0300

    from dev

--- src/test/java/Another.java
+++ src/test/java/Another.java
@@ -2,5 +2,5 @@ public class Another {
 
     String s = "first";
     String s2 = "second";
-    String s4 = "four";
+    String s3 = "third";
 }
--- src/test/java/Glav.java
+++ src/test/java/Glav.java
@@ -1,6 +1,6 @@
 public class Glav {
 
     public static void main(String[] args) {
-        String froGlav = "rugagakugaga";
+        String froGlav = "rugaga";
     }
 }
